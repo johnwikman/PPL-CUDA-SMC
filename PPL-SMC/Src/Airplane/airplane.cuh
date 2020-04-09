7705ea9d11b41ebdc3f3333dd562ca889eb30d2e
John Wikman <john.wikman@hotmail.com>
2020-04-09 19:01:14 +0200

    temp test

--- PPL-SMC/Src/Airplane/airplane.cuh
+++ PPL-SMC/Src/Airplane/airplane.cuh
@@ -15,7 +15,7 @@ const int TRANSITION_STD = 1;
 const int VELOCITY = 2;
 const int MAP_SIZE = 201;
 const int ALTITUDE = 70;
-const int TIME_STEPS = 100;
+const int TIME_STEPS = 75;
 
 const floating_t STARTING_POINT = 20.0;
 const floating_t SQRT_TWO_PI = 2.506628274631000502415765284811045253006986740609938316629;

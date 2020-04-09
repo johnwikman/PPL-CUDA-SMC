07d6997946f0e8454af8e5615b3167a8c9055388
John Wikman <john.wikman@hotmail.com>
2020-04-09 18:54:02 +0200

    Modified airplane to use precomputed values.

--- PPL-SMC/Src/Airplane/airplane.cuh
+++ PPL-SMC/Src/Airplane/airplane.cuh
@@ -12,12 +12,12 @@ typedef progState_t stateType;
 
 const int OBSERVATION_STD = 1;
 const int TRANSITION_STD = 1;
-const int VELOCITY = 1;
-const int MAP_SIZE = 200;
-const int ALTITUDE = 10;
-const int TIME_STEPS = 75;
+const int VELOCITY = 2;
+const int MAP_SIZE = 201;
+const int ALTITUDE = 70;
+const int TIME_STEPS = 100;
 
-const floating_t STARTING_POINT = 5.0;
+const floating_t STARTING_POINT = 20.0;
 const floating_t SQRT_TWO_PI = 2.506628274631000502415765284811045253006986740609938316629;
 const floating_t SQRT_TWO_PI_OBS_STD = SQRT_TWO_PI * OBSERVATION_STD;
 const floating_t TWO_OBS_STD_SQUARED = OBSERVATION_STD * OBSERVATION_STD * 2;
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# PPL-CUDA-SMC
+# PPL-CUDA-SMC (Forked version used for MCore benchmarks)
+
+
 
 Contains various programs related to Probabilistic programming, CUDA and/or Sequential Monte Carlo
